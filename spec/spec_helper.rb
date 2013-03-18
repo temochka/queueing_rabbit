88419da0c3c73da7de0421d2b7dec8b0cc425977
Artem Chistyakov <tema@wildbit.com>
2013-03-18 18:31:46 +0800

    Add configuration block for integration tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,5 +13,10 @@ require 'support/shared_examples'
 
 require 'queueing_rabbit'
 
-RSpec.configure do |config|  
+RSpec.configure do |config|
+  QueueingRabbit.configure do |qr|
+    qr.amqp_uri = "amqp://guest:guest@localhost:5672"
+    qr.amqp_exchange_name = "queueing_rabbit_test"
+    qr.amqp_exchange_options = {:durable => true}
+  end
 end
