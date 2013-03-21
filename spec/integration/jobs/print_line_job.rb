1d710585bca30f6a289b6ddbd92f45f57d511691
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-03-21 16:58:10 +0800

    Update test PrintLineJob with the default value for io.

--- spec/integration/jobs/print_line_job.rb
+++ spec/integration/jobs/print_line_job.rb
@@ -2,12 +2,16 @@ class PrintLineJob
   extend QueueingRabbit::Job
 
   class << self
-    attr_accessor :io
+    attr_writer :io
   end
 
   queue :print_line_job, :durable => true
 
   def self.perform(arguments = {})
     self.io.puts arguments[:line]
+  end
+
+  def self.io
+    @io ||= STDOUT
   end
 end
