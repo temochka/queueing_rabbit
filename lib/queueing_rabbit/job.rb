b9621e7da52dd5f8e79495e239a3c47f5d1dc295
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-08-15 23:38:26 +0800

    New Jobs API.

--- lib/queueing_rabbit/job.rb
+++ lib/queueing_rabbit/job.rb
@@ -1,5 +1,14 @@
 module QueueingRabbit
   module Job
+
+    def queue(*args)
+      if args.first.kind_of?(Hash)
+        @queue_options = args.first
+      else
+        @queue_name, @queue_options = args
+      end
+    end
+
     def queue_name
       @queue_name ||= self.name.split('::')[-1]
     end
@@ -8,25 +17,112 @@ module QueueingRabbit
       @queue_options ||= {}
     end
 
-    def queue(name, options = {})
-      @queue_name = name
-      @queue_options = options
-    end
-
     def queue_size
       QueueingRabbit.queue_size(self)
+    end
+
+    def channel(options={})
+      @channel_options = options
     end
 
     def channel_options
       @channel_options ||= {}
     end
 
-    def channel(options={})
-      @channel_options = options
+    def exchange(name, options = {})
+      @exchange_name = name
+      @exchange_options = options
+    end
+
+    def exchange_name
+      @exchange_name
+    end
+
+    def exchange_options
+      @exchange_options ||= {}
+    end
+
+    def bind(options = {})
+      @binding_options = options
+    end
+
+    def binding_options
+      @binding_options || nil
+    end
+
+    def bind_queue?
+      exchange_options[:type] && exchange_options[:type] != :default && binding_options
+    end
+
+    def enqueue(payload, options = {})
+      QueueingRabbit.enqueue(self, payload, options)
+    end
+  end
+
+  module JobExtensions
+    module DirectExchange
+      def self.included(klass)
+        klass.extend ClassMethods
+      end
+
+      module ClassMethods
+        def exchange_options
+          @exchange_options ||= {}
+          @exchange_options.update(:type => :direct)
+        end
+
+        def binding_options
+          @binding_options || {:key => queue_name}
+        end
+      end
+    end
+
+    module Retryable
+      def retries
+        headers[:qr_retries].to_i
+      end
+
+      def retry_upto(max_retries)
+        if retries < max_retries
+          updated_headers = headers.update(:qr_retries => retries + 1)
+          self.class.enqueue(payload, :headers => updated_headers)
+        end
+      end
     end
   end
 
   class AbstractJob
     extend Job
+    attr_reader :payload, :metadata
+
+    def initialize(payload, metadata)
+      @payload = payload
+      @metadata = metadata
+    end
+
+    def acknowledge
+      metadata.ack
+    end
+
+    def headers
+      metadata.headers
+    end
+
+    def perform
+    end
+  end
+
+  class JSONJob < AbstractJob
+    extend QueueingRabbit::Serializer
+
+    alias_method :arguments, :payload
+
+    def self.enqueue(payload, metadata = {})
+      super serialize(payload), metadata
+    end
+
+    def initialize(payload, metadata = {})
+      super self.class.deserialize(payload), metadata
+    end
   end
 end
--- spec/unit/queueing_rabbit/job_spec.rb
+++ spec/unit/queueing_rabbit/job_spec.rb
@@ -1,15 +1,33 @@
 require 'spec_helper'
 
 describe QueueingRabbit::AbstractJob do
-  subject { QueueingRabbit::AbstractJob }
-  
+  let(:job_class) {
+    Class.new(QueueingRabbit::AbstractJob) do
+      queue 'test_queue', :durable => true
+      exchange 'test_exchange', :durable => false
+      bind :key => 'test.*'
+    end
+  }
+
+  subject { job_class }
+
+  it { should respond_to(:exchange).with(1).argument }
+  it { should respond_to(:exchange).with(2).arguments }
+  it { should respond_to(:exchange_name) }
+  it { should respond_to(:exchange_options) }
+  it { should respond_to(:queue).with(1).argument }
   it { should respond_to(:queue).with(2).arguments }
   it { should respond_to(:queue_name) }
   it { should respond_to(:queue_options) }
   it { should respond_to(:channel_options) }
   it { should respond_to(:channel).with(1).argument }
+  it { should respond_to(:enqueue).with(1).argument }
+  it { should respond_to(:enqueue).with(2).arguments }
 
-  its(:queue_name) { should == 'AbstractJob' }
+  its(:queue_name) { should == 'test_queue' }
+  its(:queue_options) { should include(:durable => true) }
+  its(:exchange_options) { should include(:durable => false) }
+  its(:binding_options) { should include(:key => 'test.*') }
 
   describe ".queue_size" do
     let(:size) { mock }
@@ -20,4 +38,97 @@ describe QueueingRabbit::AbstractJob do
 
     its(:queue_size) { should == size }
   end
+
+  describe '.enqueue' do
+    let(:payload) { mock }
+    let(:options) { mock }
+
+    it 'enqueues a job of its own type with given argument' do
+      QueueingRabbit.should_receive(:enqueue).with(subject, payload, options)
+      subject.enqueue(payload, options)
+    end
+  end
+
+  context 'instance methods' do
+    let(:payload) { mock }
+    let(:headers) { mock }
+    let(:metadata) { stub(headers: headers) }
+
+    subject { job_class.new(payload, metadata) }
+
+    its(:payload) { should == payload }
+    its(:metadata) { should == metadata }
+    its(:headers) { should == headers }
+
+    it { should respond_to(:perform) }
+  end
+end
+
+describe QueueingRabbit::JSONJob do
+  let(:json_job) { QueueingRabbit::JSONJob }
+  let(:payload) { JSON.dump(:foo => 'bar') }
+  let(:metadata) { mock }
+
+  subject { json_job.new(payload, metadata) }
+
+  its(:payload) { should include(:foo => 'bar') }
+
+  describe '.enqueue' do
+    let(:options) { mock }
+
+    it 'dumps payload to JSON' do
+      QueueingRabbit.should_receive(:enqueue).with(json_job, payload, options)
+      json_job.enqueue({:foo => 'bar'}, options)
+    end
+  end
+end
+
+describe QueueingRabbit::JobExtensions::DirectExchange do
+  let(:test_job) {
+    Class.new(QueueingRabbit::AbstractJob) do
+      include QueueingRabbit::JobExtensions::DirectExchange
+
+      exchange 'test_job'
+      queue 'test_queue'
+    end
+  }
+
+  subject { test_job }
+
+  its(:exchange_name) { should == 'test_job' }
+  its(:exchange_options) { should include(:type => :direct) }
+  its(:binding_options) { should include(:key => 'test_queue') }
+end
+
+describe QueueingRabbit::JobExtensions::Retryable do
+  let(:test_job) {
+    Class.new(QueueingRabbit::AbstractJob) do
+      include QueueingRabbit::JobExtensions::Retryable
+
+      exchange 'test_job'
+      queue 'test_queue'
+
+      def perform
+      end
+    end
+  }
+  let(:payload) { mock }
+  let(:headers) { {:qr_retries => 2} }
+  let(:metadata) { mock(headers: headers)}
+
+  subject { test_job.new(payload, metadata) }
+
+  its(:retries) { should == 2 }
+
+  describe '#retry_upto' do
+    it 'returns nil and does not retry if attempts exceeded' do
+      subject.retry_upto(2).should_not be
+    end
+
+    it 'retries with increased number of attempts' do
+      test_job.should_receive(:enqueue).with(payload, headers: {:qr_retries => 3}).and_return(true)
+      subject.retry_upto(3).should be_true
+    end
+  end
+
 end
