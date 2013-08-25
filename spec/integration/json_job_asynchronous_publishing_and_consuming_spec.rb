f2d98615eb8e7f5bad33b3318e2ed4a3fad77ea5
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-08-25 15:35:33 +0800

    Add an integration test to test JSON serialized jobs.

---
+++ spec/integration/json_job_asynchronous_publishing_and_consuming_spec.rb
@@ -0,0 +1,99 @@
+require 'spec_helper'
+require 'integration/jobs/print_line_job'
+
+describe "Asynchronous publishing and consuming example" do
+  include_context "Evented spec"
+  include_context "StringIO logger"
+
+  before(:all) { QueueingRabbit.client = QueueingRabbit::Client::AMQP }
+  after(:all) { QueueingRabbit.client = QueueingRabbit.default_client }
+
+  context "basic consuming" do
+    let(:line) { "Hello, world!" }
+    let(:connection) { QueueingRabbit.connection }
+    let(:job) {
+      Class.new(QueueingRabbit::JSONJob) do
+        class << self
+          attr_accessor :io
+        end
+
+        def perform
+          self.class.io.puts arguments[:line]
+        end
+      end
+
+    }
+    let(:job_name) { 'PrintLineFromJSONJob' }
+    let(:io) { StringIO.new }
+    let(:worker) { QueueingRabbit::Worker.new(job_name) }
+
+    before do
+      stub_const(job_name, job)
+      job.io = io
+    end
+
+    before(:each) do
+      QueueingRabbit.drop_connection
+    end
+
+    before do
+      job.io = io
+    end
+
+    it "processes enqueued jobs" do
+      em {
+        QueueingRabbit.connect
+        @queue_size = nil
+
+        def request_queue_size
+          connection.open_channel do |c, _|
+            connection.define_queue(c, job.queue_name, job.queue_options).status do |s, _|
+              @queue_size = s
+            end
+          end
+        end
+
+        delayed(0.5) {
+          3.times { job.enqueue(:line => line) }
+        }
+
+        delayed(1.5) {
+          request_queue_size
+        }
+
+        delayed(2.0) {
+          @queue_size.should == 3
+        }
+
+        delayed(2.5) {
+          worker.work
+        }
+
+        delayed(3.5) {
+          request_queue_size
+        }
+
+        done(4.0) {
+          @queue_size.should be_zero
+        }
+      }
+    end
+
+    it "actually outputs the line" do
+      em {
+        QueueingRabbit.connect
+
+        delayed(0.5) { job.enqueue(:line => line) }
+
+        delayed(1.0) {
+          worker.work
+        }
+
+        done(2.0) {
+          io.string.should include(line)
+        }
+      }
+    end
+  end
+end
+
