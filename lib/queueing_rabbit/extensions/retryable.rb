8f1d0223aa52ab0d2475ab3608f917b98f45f0e7
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-08-25 16:15:44 +0800

    Use strings for header names.

--- lib/queueing_rabbit/extensions/retryable.rb
+++ lib/queueing_rabbit/extensions/retryable.rb
@@ -5,12 +5,12 @@ module QueueingRabbit
     module Retryable
 
       def retries
-        headers[:qr_retries].to_i
+        headers['qr_retries'].to_i
       end
 
       def retry_upto(max_retries)
         if retries < max_retries
-          updated_headers = headers.update(:qr_retries => retries + 1)
+          updated_headers = headers.update('qr_retries' => retries + 1)
           self.class.enqueue(payload, :headers => updated_headers)
         end
       end
