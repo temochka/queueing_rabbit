d0fd6fa50128f054fbe5466fa514f44557bc5a50
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-08-25 17:09:10 +0800

    Refactor NewRelic job extension.

--- lib/queueing_rabbit/extensions/new_relic.rb
+++ lib/queueing_rabbit/extensions/new_relic.rb
@@ -6,17 +6,25 @@ module QueueingRabbit
 
       def self.included(klass)
         if klass.respond_to?(:perform)
-          klass.class_eval do |k|
-            class << k
-              include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
-              add_transaction_tracer :perform, :category => :task
-            end
-          end
+          add_for_class_method(klass)
         else
-          klass.class_eval do |k|
+          add_for_instance_method(klass)
+        end
+      end
+
+      def self.add_for_class_method(klass)
+        klass.class_eval do |k|
+          class << k
             include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
             add_transaction_tracer :perform, :category => :task
           end
+        end
+      end
+
+      def self.add_for_instance_method(klass)
+        klass.class_eval do |k|
+          include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
+          add_transaction_tracer :perform, :category => :task
         end
       end
 
