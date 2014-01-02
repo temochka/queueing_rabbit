c36ba9978809ca9f9fd946a4ade56f23286f403c
Artem Chistyakov <tema@wildbit.com>
2014-01-02 11:44:48 -0500

    Update the 'StringIO logger' shared context to write logs into a variable.

--- spec/support/shared_contexts.rb
+++ spec/support/shared_contexts.rb
@@ -3,11 +3,28 @@ require 'stringio'
 shared_context "StringIO logger" do
 
   before(:all) do
-    QueueingRabbit.logger = Logger.new(StringIO.new)
+    @session_log = StringIO.new
+    QueueingRabbit.logger = Logger.new(@session_log)
   end
 
   after(:all) do
     QueueingRabbit.logger = Logger.new(STDOUT)
+  end
+
+end
+
+shared_context "Auto-disconnect" do
+
+  after(:each) do
+    QueueingRabbit.disconnect
+  end
+
+end
+
+shared_context "No existing connections" do
+
+  before(:each) do
+    QueueingRabbit.drop_connection
   end
 
 end
